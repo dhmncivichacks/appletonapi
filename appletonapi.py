090e5394c20958cf9c34deb1d1bbc4892f2ac3b0
Mike Putnam <mike@theputnams.net>
2015-01-04 22:00:41 -0600

    Update copyright year.

--- appletonapi.py
+++ appletonapi.py
@@ -1,5 +1,5 @@
 """
-Copyright (c) 2013 Mike Putnam <mike@theputnams.net>
+Copyright (c) 2015 Mike Putnam <mike@theputnams.net>
 
 Permission to use, copy, modify, and distribute this software for any
 purpose with or without fee is hereby granted, provided that the above
