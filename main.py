99c625b4a8059ddde8b6aa72855425599e791635
Mike Putnam <mike@theputnams.net>
2023-02-17 11:57:31 -0600

    WIP search working

--- main.py
+++ main.py
@@ -21,12 +21,11 @@ import logging
 import re
 
 import inflect
-import lxml.etree
-import lxml.html
 import requests
 import streetaddress
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from lxml import etree, html
 
 app = Flask(__name__)
 
@@ -38,27 +37,19 @@ def contains_digits(maybe_digits):
     return bool(_digits.search(maybe_digits))
 
 
-def extracttagvalues(line):
-    '''Parse html stuff.'''
-    maybe_match = re.search(r'(?<=value\=\").*?([^\'" >]+)', line)
-    if maybe_match:
-        return re.split('value="', maybe_match.group(0))[0]
-    return None
-
-
 @app.route('/property/<int:propkey>')
 def property_handler(propkey):
     '''Calls coming in with a propkey.'''
     detailurl = "http://my.appleton.org/Propdetail.aspx?PropKey=" + str(propkey)
     datagroups = []
     try:
-        docroot = lxml.html.fromstring(requests.get(detailurl, timeout=15).content)
+        docroot = html.fromstring(requests.get(detailurl, timeout=15).content)
         tables = docroot.xpath("//table[@class='t1']")
         for table in tables:
             ths = table.xpath("./tr/th")  # assuming single <th> per table
             for th in ths:
                 if th is not None:
-                    lxml.etree.strip_tags(th, 'a', 'b', 'br', 'span', 'strong')
+                    etree.strip_tags(th, 'a', 'b', 'br', 'span', 'strong')
                     if th.text:
                         thkey = re.sub('\W', '', th.text).lower()  # nospaces all lower
                         datagroups.append(thkey)
@@ -69,7 +60,7 @@ def property_handler(propkey):
                             tds = table.xpath("./tr/td")
                             if tds is not None:
                                 for td in tds:
-                                    lxml.etree.strip_tags(td, 'a', 'b', 'br', 'span', 'strong')
+                                    etree.strip_tags(td, 'a', 'b', 'br', 'span', 'strong')
                                     businesslist.append(td.text.strip())
                             logging.debug("businesslist: " + str(businesslist))
                         datadict = {}
@@ -77,7 +68,7 @@ def property_handler(propkey):
                         tds = table.xpath("./tr/td")
                         if tds is not None:
                             for td in tds:
-                                lxml.etree.strip_tags(td, 'a', 'b', 'br', 'span', 'strong')
+                                etree.strip_tags(td, 'a', 'b', 'br', 'span', 'strong')
                                 if tdcounter == 0:
                                     tdkey = re.sub('\W', '', td.text).lower() if td.text else ''
                                     tdcounter += 1
@@ -97,95 +88,100 @@ def property_handler(propkey):
 
 
 @app.route('/search')
-class SearchHandler():
+def search_handler():
     '''
     The ugliest hack here (and only real value-add of the whole thing).
     Parse .aspx forms and get back "useful" data.
     '''
-    def get(self):
-        '''Get'''
-        return self.write_response(self.execute(self.request.get('q'), str(self.request.headers['User-Agent'])))
+    # Google maps geolocation appends 'USA' but the address parser can't cope
+    search_input = request.args['q']
+    search_input = search_input.replace('USA','')
+    addr = streetaddress.parse(search_input)
+    if addr is None:
+        # Since we are so tightly coupled with Appleton data,
+        # let's just pacify the address parser.
+        addr = streetaddress.parse(search_input + ' Appleton, WI')
+    housenumber = addr['number']
+    # Handle upstream requirement of "Fifth" not "5th"
+    inflect_instance = inflect.engine()
+    if contains_digits(addr['street']):
+        street = inflect_instance.number_to_words(addr['street'])
+    else:
+        street = addr['street']
 
-    def execute(self, search_input, user_agent):
-        '''Execute'''
-        # Google maps geolocation appends 'USA' but the address parser can't cope
-        search_input = search_input.replace('USA','')
-        addr = streetaddress.parse(search_input)
-        if addr is None:
-            # Since we are so tightly coupled with Appleton data,
-            # let's just pacify the address parser.
-            addr = streetaddress.parse(search_input + ' Appleton, WI')
-        housenumber = addr['number']
-        # Handle upstream requirement of "Fifth" not "5th"
-        inflect_instance = inflect.engine()
-        if contains_digits(addr['street']):
-            street = inflect_instance.number_to_words(addr['street'])
-        else:
-            street = addr['street']
+    if not housenumber and not street:
+        return { 'error' : 'Give me *SOMETHING* to search for.'}
 
-        if not housenumber and not street:
-            return { 'error' : 'Give me *SOMETHING* to search for.'}
+    try:
+        foo = requests.get('http://my.appleton.org/', timeout=15).text
+        foo_parser = etree.HTMLParser()
+        foo_tree = etree.fromstring(foo, foo_parser)
+        allresults = []
+        #print(foo)
+        view_state = foo_tree.xpath("//input[@name='__VIEWSTATE']/@value")[0]
+        event_validation = foo_tree.xpath("//input[@name='__EVENTVALIDATION']/@value")[0]
 
-        try:
-            response = requests.get('http://my.appleton.org/', timeout=15).content
-            for line in response:
-                if "__VIEWSTATE\"" in line:
-                    vs = extracttagvalues(line)
-                if "__EVENTVALIDATION\"" in line:
-                    ev = extracttagvalues(line)
-                    formvalues = {
-                        '__EVENTTARGET': '',
-                        '__EVENTARGUMENT': '',
-                        '__VIEWSTATE': vs,
-                        '__EVENTVALIDATION': ev,
-                        'ctl00$myappletonContent$txtStreetNumber': housenumber,
-                        'ctl00$myappletonContent$txtStreetName': street,
-                        'ctl00$myappletonContent$btnSubmit': 'Submit'}
-                    headers = {
-                        'User-Agent': user_agent,
-                        'Referer': 'http://my.appleton.org/default.aspx',
-                        'Accept': 'text/html,application/xhtml+xml,application/xml'
-                    }
-                    #FIXME This is going to hurt.  was "urllib2" >>> now make it "requests".
-                    data = six.moves.urllib.parse.urlencode(formvalues)
-                    req = six.moves.urllib.request.Request("http://my.appleton.org/default.aspx", data, headers)
-                    response = six.moves.urllib.request.urlopen(req)
-                    allresults = []
-                    # Example of the HTML returned...
-                    # <a id="ctl00_myappletonContent_searchResults_ctl03_PropKey"
-                    # href="Propdetail.aspx?PropKey=312039300&amp;Num=100">312039300  </a>
-                    #                  </td><td>100</td><td>E WASHINGTON           ST  </td>
-                    for pline in response:
-                        if "Propdetail.aspx?PropKey=" in pline:
-                            searchresult = []
-                            maybe_propkey_match = re.search(r'(?<=PropKey\=).*(?=&)', pline)
-                            if maybe_propkey_match:
-                                searchresult.append(
-                                    re.split('PropKey=', maybe_propkey_match.group(0))[0]
-                                    )
-                            maybe_td_match = re.findall(r'(?s)<td>(.*?)</td>', next(response))
-                            if maybe_td_match:
-                                # this removes whitespace and Title Cases the address
-                                # given: <td>1200</td><td>W WISCONSIN    AVE </td>
-                                # returns: ['1200', 'W Wisconsin Ave']
-                                address = [
-                                    ' '.join(t.split()).strip().title() for t in maybe_td_match
-                                    ]
-                                searchresult.append(address[0]) #Number
-                                # Thank you Dan Gabrielson <dan.gabrielson@gmail.com> and
-                                # Matt Everson https://github.com/matteverson
-                                # for your help at 2015 Appleton Civic Hackathon!
-                                # This closes https://github.com/mikeputnam/appletonapi/issues/5
-                                label = ' '
-                                for chunk in address[1:]:
-                                    label += chunk + ' '
-                                searchresult.append(label.strip())
-                            allresults.append(searchresult)
+        formvalues = {
+            '__EVENTTARGET': '',
+            '__EVENTARGUMENT': '',
+            '__VIEWSTATE': view_state,
+            '__EVENTVALIDATION': event_validation,
+            'ctl00$myappletonContent$txtStreetNumber': housenumber,
+            'ctl00$myappletonContent$txtStreetName': street,
+            'ctl00$myappletonContent$btnSubmit': 'Submit'}
+        headers = {
+            'User-Agent': request.headers['User-Agent'],
+            'Referer': 'http://my.appleton.org/default.aspx',
+            'Accept': 'text/html,application/xhtml+xml,application/xml'
+        }
+        response = requests.post(
+            "http://my.appleton.org/default.aspx",
+            headers=headers,
+            data=formvalues,
+            timeout=15
+            )
+        #print(response.text)
+        res_parser = etree.HTMLParser()
+        res_tree = etree.fromstring(response.text, res_parser)
+        # Example of the HTML returned...
+        # <a id="ctl00_myappletonContent_searchResults_ctl03_PropKey"
+        # href="Propdetail.aspx?PropKey=312039300&amp;Num=100">312039300  </a>
+        #                  </td><td>100</td><td>E WASHINGTON           ST  </td>
+        searchresult = []
+        stripped_propkey = res_tree.xpath("//a[contains(@id, 'ctl00_myappletonContent_searchResults_ctl03_PropKey')]/text()")[0].strip()
+        searchresult.append(stripped_propkey)
+        # for pline in response:
+        #     if "Propdetail.aspx?PropKey=" in pline:
+        #         searchresult = []
+        #         maybe_propkey_match = re.search(r'(?<=PropKey\=).*(?=&)', pline)
+        #         if maybe_propkey_match:
+        #             searchresult.append(
+        #                 re.split('PropKey=', maybe_propkey_match.group(0))[0]
+        #                 )
+        #         maybe_td_match = re.findall(r'(?s)<td>(.*?)</td>', next(response))
+        #         if maybe_td_match:
+        #             # this removes whitespace and Title Cases the address
+        #             # given: <td>1200</td><td>W WISCONSIN    AVE </td>
+        #             # returns: ['1200', 'W Wisconsin Ave']
+        #             address = [
+        #                 ' '.join(t.split()).strip().title() for t in maybe_td_match
+        #                 ]
+        #             searchresult.append(address[0]) #Number
+        #             # Thank you Dan Gabrielson <dan.gabrielson@gmail.com> and
+        #             # Matt Everson https://github.com/matteverson
+        #             # for your help at 2015 Appleton Civic Hackathon!
+        #             # This closes https://github.com/mikeputnam/appletonapi/issues/5
+        #             label = ' '
+        #             for chunk in address[1:]:
+        #                 label += chunk + ' '
+        #             searchresult.append(label.strip())
+        #         allresults.append(searchresult)
+        allresults.append(searchresult)
 
-            return { 'result' : allresults }
-        except RuntimeError as err:
-            logging.error('SEARCH FAIL! my.appleton.org up? scrape assumptions still valid?')
-            return { 'error' : "Cannot search :( <br/>" + str(err) }
+        return { 'result' : allresults }
+    except RuntimeError as err:
+        logging.error('SEARCH FAIL! my.appleton.org up? scrape assumptions still valid?')
+        return { 'error' : "Cannot search :( <br/>" + str(err) }
 
 
 @app.route('/garbagecollection')
@@ -204,8 +200,7 @@ class GarbageCollectionHandler():
         collection_days = []
 
         if len(search_response['result']) > 0:
-            prop_handler = PropertyHandler()
-            prop_response = prop_handler.execute(search_response['result'][0][0])
+            prop_response = property_handler(search_response['result'][0][0])
             if 'error' in prop_response:
                 return prop_response
 
@@ -271,4 +266,3 @@ def main_handler():
 </html>
     """
     return indexhtml
-
